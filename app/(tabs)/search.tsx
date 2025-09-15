e366fc9bf94adc9220ae2f7862a468f72ba25b5a
ye21iin <go_120@naver.com>
2025-09-15 23:24:22 +0900

    fix: match City type

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -3,40 +3,46 @@ import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { useCities } from "@/context/CityContext";
 import React, { useState } from "react";
-import { Button, FlatList, StyleSheet, TextInput, useColorScheme } from "react-native";
+import {
+  Button,
+  FlatList,
+  StyleSheet,
+  TextInput,
+  useColorScheme,
+} from "react-native";
 
 const DATA = [
-  { id: "1", name: "Seoul", country: "South Korea", timezone: "Asia/Seoul" },
-  { id: "2", name: "New York", country: "USA", timezone: "America/New_York" },
-  { id: "3", name: "London", country: "UK", timezone: "Europe/London" },
-  { id: "4", name: "Tokyo", country: "Japan", timezone: "Asia/Tokyo" },
+  { id: "1", city: "Seoul", country: "South Korea", timezone: "Asia/Seoul" },
+  { id: "2", city: "New York", country: "USA", timezone: "America/New_York" },
+  { id: "3", city: "London", country: "UK", timezone: "Europe/London" },
+  { id: "4", city: "Tokyo", country: "Japan", timezone: "Asia/Tokyo" },
   {
     id: "5",
-    name: "Vancouver",
+    city: "Vancouver",
     country: "Canada",
     timezone: "America/Vancouver",
   },
-  { id: "6", name: "Toronto", country: "Canada", timezone: "America/Toronto" },
+  { id: "6", city: "Toronto", country: "Canada", timezone: "America/Toronto" },
   {
     id: "7",
-    name: "Sydney",
+    city: "Sydney",
     country: "Australia",
     timezone: "Australia/Sydney",
   },
-  { id: "8", name: "Paris", country: "France", timezone: "Europe/Paris" },
+  { id: "8", city: "Paris", country: "France", timezone: "Europe/Paris" },
 ];
 
 export default function SearchRegion() {
   const [query, setQuery] = useState("");
   const [filteredData, setFilteredData] = useState(DATA);
   const colorScheme = useColorScheme();
-  const {addCity} = useCities();
+  const { addCity } = useCities();
 
   /** Define function onSearch */
   const handleSearch = (text: string) => {
     setQuery(text);
     const results = DATA.filter((item) =>
-      item.name.toLowerCase().includes(text.toLowerCase())
+      item.city.toLowerCase().includes(text.toLowerCase())
     );
     setFilteredData(results);
   };
@@ -62,8 +68,8 @@ export default function SearchRegion() {
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
           <ThemedView style={styles.row}>
-            <ThemedText style={styles.item}>{item.name}</ThemedText>
-            <Button title="추가" onPress={() => addCity(item)} />
+            <ThemedText style={styles.item}>{item.city}</ThemedText>
+            <Button title="Add" onPress={() => addCity(item)} />
           </ThemedView>
         )}
       />
@@ -87,7 +93,8 @@ const styles = StyleSheet.create({
     padding: 10,
     marginBottom: 20,
     borderRadius: 8,
-  }, row: {
+  },
+  row: {
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
